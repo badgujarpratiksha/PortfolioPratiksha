2d88a8208bc20d22c15830e94d9694f6a05df9c2
Aniket Dalal <58764738+aniket691@users.noreply.github.com>
2024-04-11 01:59:03 +0530

    responsive design

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,9 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 const Header = () => {
   const headerRef = useRef(null);
   const menuRef = useRef(null);
-
+  const [isMenuOpen, setMenuOpen] = useState(false);
 
   const stickyHeaderFunc = () => {
     window.addEventListener('scroll', () => {
@@ -26,7 +26,6 @@ const Header = () => {
   const handleClick = (e) => {
     e.preventDefault();
     const targetAttr = e.target.getAttribute('href');
-
     const targetElement = document.querySelector(targetAttr);
 
     if (targetElement) {
@@ -35,64 +34,69 @@ const Header = () => {
       window.scrollTo({
         top: location - 80,
         left: 0,
-        behavior: 'smooth' // Add smooth scrolling effect
+        behavior: 'smooth'
       });
     }
   };
-  const toggleMenu =() => menuRef.current.classList.toggle('show_menu');
 
+  const toggleMenu = () => setMenuOpen(!isMenuOpen);
 
   return (
     <header
       ref={headerRef}
-      className="w-full h-[80px] leading-[80px] flex items-center">
-        {/* logo */}
-      <div className="container" >
-        <div className="flex items-center justify-between ">
-          <div className="flex items-center gap-[10px] ">
-            <span className="w-[35px] h-[35px] bg-blue-950 text-white text[18px] font-[500] rounded-full flex items-center justify-center">
+      className={`w-full h-[80px] leading-[80px] flex items-center ${
+        isMenuOpen ? 'menu-open' : ''
+      }`}
+    >
+      {/* Logo */}
+      <div className="container">
+        <div className="flex items-center justify-between">
+          <div className="flex items-center gap-[10px]">
+            <span className="w-[35px] h-[35px] bg-blue-950 text-white text-[18px] font-[500] rounded-full flex items-center justify-center">
               PB
             </span>
             <div className="leading-[20px]">
-              <h2 className="text-xl text-smallTextColor font-[700] uppercase">Pratiksha</h2>
+              <h2 className="text-xl text-smallTextColor font-[700] uppercase">
+                Pratiksha
+              </h2>
               <p className="text-smallTextColor text-[16px] font-[500] uppercase">
                 Badgujar
               </p>
             </div>
           </div>
-          {/* logoend */}
-          {/* menu */}
+          {/* Menu */}
           <div className="menu" onClick={toggleMenu} ref={menuRef}>
-            <ul className="flex item-center gap-[20px]">
+            <ul className="flex gap-[20px]">
               <li>
-                <a onClick={handleClick}   href="#about" className="text-smallTextColor font-[600]">
+                <a href="#home" onClick={handleClick}>
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#about" onClick={handleClick}>
                   About
                 </a>
               </li>
               <li>
-                <a onClick={handleClick} href="#services" className="text-smallTextColor font-[600]">
-                Education
+                <a href="#services" onClick={handleClick}>
+                  Services
                 </a>
               </li>
               <li>
-                <a onClick={handleClick} href="#Portfolio" className="text-smallTextColor font-[600]">
-                Certification
+                <a href="#portfolio" onClick={handleClick}>
+                  Portfolio
                 </a>
               </li>
               <li>
-                <a onClick={handleClick} href="#contact" className="text-smallTextColor font-[600]">
+                <a href="#contact" onClick={handleClick}>
                   Contact
                 </a>
               </li>
             </ul>
           </div>
-          <div className="flex items-center gap-4">
-            <button className="flex items-center gap-2 text-smallTextColor font-[600] border border-solid border-smallTextColor py-2 px-4 rounded-[8px] max-h-[40px] hover:bg-smallTextColor hover:text-white hover:font-[500] easy-in duration-700">
-              <i className="ri-send-plane-fill"></i> Let's talk
-            </button>
-            <span onClick={toggleMenu} className="text-2xl text-smallTextColor cursor-pointer">
-              <i className="ri-menu-line"></i>
-            </span>
+          {/* Menu End */}
+          <div className="mobile-menu-icon" onClick={toggleMenu}>
+            <i className="ri-menu-line"></i>
           </div>
         </div>
       </div>
